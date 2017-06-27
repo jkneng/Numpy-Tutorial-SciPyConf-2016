0aa0894a87e947e7660998f166abe86b660c5308
nenggong <neng.gong@qq.com>
2017-06-27 19:09:10 +0800

    add derivative

--- exercises/calc_derivative/calc_derivative.py
+++ exercises/calc_derivative/calc_derivative.py
@@ -26,5 +26,24 @@ from matplotlib.pyplot import plot, show, subplot, legend, title
 x = linspace(0,2*pi,101)
 y = sin(x)
 
-plot(x,y)
+dx = x[1:] - x[:-1]
+dy = y[1:] - y[:-1]
+dy_dx = dy/dx
+
+centers_x = (x[1:] + x[:-1]) / 2
+subplot(1, 2, 1)
+plot(centers_x, dy_dx, 'rx', centers_x, cos(centers_x), 'b-')
+title(r"$\rm{Derivative\ of}\ sin(x)$")
+
+# Bonus: integration of sinx: Riemann sums or the trapezoidal rule
+avg_height = (y[1:] + y[:-1]) / 2
+integral_sin = cumsum(avg_height * dx)
+
+close_form = -cos(x) + 1
+subplot(1, 2, 2)
+
+plot(x[1:], integral_sin, 'rx', x, close_form, 'b-')
+legend(('numerical', 'actual'))
+title(r"$\int \, \sin(x) \, dx$")
+
 show()
